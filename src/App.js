2157ab47290b2e366f65b9ffa8da505c82a6ec8d
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-29 08:48:07 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './component/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Post from './pages/Post';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
